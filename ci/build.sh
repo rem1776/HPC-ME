887dd2dfc49470e4e6ce528840b2cc11e271b51a
rem1776 <Ryan.Mulhall@noaa.gov>
2026-03-19 17:00:54 -0400

    some fixes, add in error checks for builds

--- ci/build.sh
+++ ci/build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 podman build --no-cache . --file=Dockerfile-base -t "arch-gnu-openmpi-base"
+test $? = 0 || { echo "base container build failed, exiting..."; exit 1;}
 
 podman build --no-cache . --file=Dockerfile-fms -t "fms-gnu-ci"
+test $? = 0 || { echo "fms container build failed, exiting..."; exit 1;}
 
 podman build --no-cache . --file=Dockerfile-frenct -t "frenct-gnu-ci"
+test $? = 0 || { echo "fre-nctools container build failed, exiting..."; exit 1;}
 
 podman build --no-cache . --file=Dockerfile-frecli -t "frecli-gnu-ci"
+test $? = 0 || { echo "frecli container build failed, exiting..."; exit 1;}
